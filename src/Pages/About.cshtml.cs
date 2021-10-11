819b6ed8e49b70d04e27bb34af4d9bf6a78c17d9
khometit <averydinh1996@gmail.com>
2021-10-10 18:59:44 -0700

    Added an About Us page + team name and members. Still need to adjust the space between buttons.

---
+++ src/Pages/About.cshtml.cs
@@ -0,0 +1,21 @@
+﻿
+
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace ContosoCrafts.WebSite.Pages
+{
+    public class AboutModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public AboutModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
