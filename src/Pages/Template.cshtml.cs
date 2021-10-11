90a4b1f0bd65a5237be59efe76f927451a3eeb6f
Alicia Garcia <garcia.alicia1990@gmail.com>
2021-10-10 22:08:44 -0700

    Adding page template and updates to index.csthml.cs

---
+++ src/Pages/Template.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+
+namespace ContosoCrafts.WebSite.Pages
+{
+
+    /// <summary>
+    /// Zhuojing Xie
+    /// Alicia Garcia
+    /// Carlo Provenzani
+    /// Avery Dinh
+    /// </summary>
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger,
+            JsonFileProductService productService)
+        {
+            _logger = logger;
+            ProductService = productService;
+        }
+
+        public JsonFileProductService ProductService { get; }
+        public IEnumerable<ProductModel> Products { get; private set; }
+
+        public void OnGet()
+        {
+            Products = ProductService.GetProducts();
+        }
+    }
+}
