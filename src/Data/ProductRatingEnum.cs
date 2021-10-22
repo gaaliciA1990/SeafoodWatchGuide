6ea57eb908836d5106a58ae53b1327b74ac46d33
Alicia Garcia <garcia.alicia1990@gmail.com>
2021-10-21 23:22:16 -0700

    Created enum class for holding product ratings. ProductRatingEnum.cs

---
+++ src/Data/ProductRatingEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoCrafts.WebSite.Data
+{
+
+    enum Product_Rating
+    {
+        BEST_CHOICE,
+        GOOD_ALTERNATIVE,
+        AVOID
+    }
+}
