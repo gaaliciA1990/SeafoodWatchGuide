0fccf74affac02d961da4c1ec07a26688fbf0696
ZhuojingXie <llxllxzj@gmail.com>
2021-10-23 01:10:04 -0700

    Created new UT file JsonFileProductServiceTest.cs

---
+++ UnitTests/Services/JsonFileProductServiceTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+
+namespace UnitTests.Services
+{
+    class JsonFileProductServiceTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+
+    }
+}
