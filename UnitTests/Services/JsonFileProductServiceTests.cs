9f479dd15af98f027d2bc743cb17717794b25f32
ZhuojingXie <llxllxzj@gmail.com>
2021-10-25 22:20:40 -0700

    Added CreateCard_Valid_Product_Should_Return_True() in JsonFileProductServiceTests.cs

--- UnitTests/Services/JsonFileProductServiceTests.cs
+++ UnitTests/Services/JsonFileProductServiceTests.cs
@@ -5,7 +5,7 @@ using NUnit.Framework;
 using ContosoCrafts.WebSite.Pages;
 using ContosoCrafts.WebSite.Models;
 using System.Linq;
-
+using Newtonsoft.Json;
 
 namespace UnitTests.Services
 {
@@ -18,10 +18,36 @@ namespace UnitTests.Services
         }
         #endregion TestSetup
 
+
         [Test]
-        public void Test1()
+        public void CreateCard_Valid_Product_Should_Return_True()
         {
-            Assert.Pass();
+
+            // Arrange
+            var origin_data = new ProductModel()
+            {
+                Id = System.Guid.NewGuid().ToString(),
+                Region = "West Coast",
+                Image = "testing image",
+                Title = "testing title",
+                Description = "Enter URL",
+                Rating = ContosoCrafts.WebSite.Data.Product_Rating.AVOID,
+            };
+
+            //Act
+            TestHelper.ProductService.CreateCard(origin_data);
+            var dataSet = TestHelper.ProductService.GetAllData();
+
+            //Reset
+            TestHelper.ProductService.DeleteCard(origin_data.Id);
+
+            //Assert
+            var newCreate = dataSet.FirstOrDefault(m => m.Id.Equals(origin_data.Id));
+
+            var origin_data_json= JsonConvert.SerializeObject(origin_data);
+            var newCreate_json = JsonConvert.SerializeObject(newCreate);
+
+            Assert.AreEqual(origin_data_json, newCreate_json);
         }
 
     }
