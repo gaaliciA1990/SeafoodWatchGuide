31025250f385f5b3751d917deb6f71fde8fbf24a
ZhuojingXie <llxllxzj@gmail.com>
2021-10-25 22:06:20 -0700

    Updated the initial UT setting in JsonFileProductServiceTests.cs

--- UnitTests/Services/JsonFileProductServiceTests.cs
+++ UnitTests/Services/JsonFileProductServiceTests.cs
@@ -1,14 +1,22 @@
-﻿using NUnit.Framework;
+﻿using Microsoft.AspNetCore.Mvc;
+
+using NUnit.Framework;
+
+using ContosoCrafts.WebSite.Pages;
+using ContosoCrafts.WebSite.Models;
+using System.Linq;
 
 
 namespace UnitTests.Services
 {
     class JsonFileProductServiceTests
     {
+         #region TestSetup
         [SetUp]
         public void Setup()
         {
         }
+        #endregion TestSetup
 
         [Test]
         public void Test1()
