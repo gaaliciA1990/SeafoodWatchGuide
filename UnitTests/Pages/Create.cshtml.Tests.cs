f8cb09f1eacb0e2f17bf092fd6b81f09ea285054
khometit <averydinh1996@gmail.com>
2021-10-26 09:21:04 -0700

    Added a unit test file for Create page called Create.cshtml.Tests.cs

---
+++ UnitTests/Pages/Create.cshtml.Tests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using NUnit.Framework;
+using ContosoCrafts.WebSite.Pages;
+using Moq;
+
+namespace UnitTests.Pages.Create
+{
+    public class CreateTests
+    {
+
+
+    }
+}
