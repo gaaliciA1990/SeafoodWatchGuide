9e08eeed01ad13740278e75947a696c3df20f65e
Alicia Garcia <garcia.alicia1990@gmail.com>
2021-10-26 09:25:37 -0700

    Added comments to the Enum class in ProductRatingEnum.cs

--- src/Data/ProductRatingEnum.cs
+++ src/Data/ProductRatingEnum.cs
@@ -4,11 +4,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+/// <summary>
+/// This is the ENUM class for seafood ratings, which are displayed on each card and dictate
+/// which section of the guide the fish card is displayed on.
+/// </summary>
 namespace ContosoCrafts.WebSite.Data
 {
 
     public enum Product_Rating
     {
+        // EnumMember value is intened to convert the numerical ratings
+        // in the JSON file to their respective ENUM. Still not working, bug logged
         [EnumMember(Value = "3")] BEST_CHOICE,
         [EnumMember(Value = "2")] GOOD_ALTERNATIVE,
         [EnumMember(Value = "1")] AVOID
