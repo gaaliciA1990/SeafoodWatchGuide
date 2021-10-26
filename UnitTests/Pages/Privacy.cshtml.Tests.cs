13fc1d299f346b4dafd5e9553cfa523953ede818
khometit <averydinh1996@gmail.com>
2021-10-26 09:56:34 -0700

    Added a test initializer to test PrivacyModel constructor to Privacy.cshtml.Tests.cs

--- UnitTests/Pages/Privacy.cshtml.Tests.cs
+++ UnitTests/Pages/Privacy.cshtml.Tests.cs
@@ -7,6 +7,22 @@ namespace UnitTests.Pages.Privacy
 {
     public class PrivacyTests
     {
+        #region TestSetup
+        public static PrivacyModel pageModel;
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            var MockLoggerDirect = Mock.Of<ILogger<PrivacyModel>>();
+
+            pageModel = new PrivacyModel(MockLoggerDirect)
+            {
+                PageContext = TestHelper.PageContext,
+                TempData = TestHelper.TempData,
+            };
+        }
+
+        #endregion TestSetup
 
     }
 }
