4a1530429a5a73a4a0a0c6384b4b00e95953fbfd
khometit <averydinh1996@gmail.com>
2021-10-26 22:25:07 -0700

    Added comments following guidelines to Admin.cshtml.cs

--- src/Pages/Admin/Admin.cshtml.cs
+++ src/Pages/Admin/Admin.cshtml.cs
@@ -10,12 +10,17 @@ namespace ContosoCrafts.WebSite.Pages
 {
 
     /// <summary>
-    /// Template for page creation
+    /// Template for index (ie.Admin) page creation
     /// </summary>
     public class AdminModel : PageModel
     {
         private readonly ILogger<AdminModel> _logger;
 
+        /// <summary>
+        /// Constructor method for Admin Page
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="productService"></param>
         public AdminModel(ILogger<AdminModel> logger, JsonFileProductService productService)
         {
             _logger = logger;
@@ -25,10 +30,21 @@ namespace ContosoCrafts.WebSite.Pages
         public JsonFileProductService ProductService { get; }
         public IEnumerable<ProductModel> Products { get; private set; }
 
+        /// <summary>
+        /// Initializing method for Admin page. All products are shown.
+        /// </summary>
         public void OnGet()
         {
             Products = ProductService.GetAllData();
         }
+
+
+
+        /// <summary>
+        /// Method to react to an action of clicking on the Create button
+        /// on the Admin page. User gets redirected to Create page.
+        /// </summary>
+        /// <returns></returns>
         public IActionResult OnGetCreate()
         {
             return RedirectToPage("Create");
