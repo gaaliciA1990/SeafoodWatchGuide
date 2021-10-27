b3c4f9ec5edc16559abbc62e480b795dbc19b799
khometit <averydinh1996@gmail.com>
2021-10-26 22:59:22 -0700

    Added comments in Error.cshtml.cs

--- src/Pages/Error.cshtml.cs
+++ src/Pages/Error.cshtml.cs
@@ -1,11 +1,13 @@
 using System.Diagnostics;
-// COMMENT BY CARLO
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
 namespace ContosoCrafts.WebSite.Pages
 {
+    /// <summary>
+    /// Model for error page
+    /// </summary>
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public class ErrorModel : PageModel
     {
@@ -15,11 +17,20 @@ namespace ContosoCrafts.WebSite.Pages
 
         private readonly ILogger<ErrorModel> _logger;
 
+
+        /// <summary>
+        /// Constructor for Error page
+        /// </summary>
+        /// <param name="logger"></param>
         public ErrorModel(ILogger<ErrorModel> logger)
         {
             _logger = logger;
         }
 
+
+        /// <summary>
+        /// Method to fetch error stack trace
+        /// </summary>
         public void OnGet()
         {
             RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
