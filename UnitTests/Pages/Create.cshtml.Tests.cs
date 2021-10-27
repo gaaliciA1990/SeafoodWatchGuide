d5dbbe1eaa95abf74005fd2ed9793fa5d90ecdeb
khometit <averydinh1996@gmail.com>
2021-10-26 23:14:36 -0700

    Added comments to Create.cshtml.Tests.cs

--- UnitTests/Pages/Create.cshtml.Tests.cs
+++ UnitTests/Pages/Create.cshtml.Tests.cs
@@ -12,12 +12,18 @@ using Moq;
 
 namespace UnitTests.Pages.Create
 {
+    /// <summary>
+    /// Class containing all unit tests for Create page model
+    /// </summary>
     public class CreateTests
     {
 
         #region TestSetup
         public static CreateModel pageModel;
 
+        /// <summary>
+        /// Test constructor
+        /// </summary>
         [SetUp]
         public void TestInitialize()
         {
@@ -31,6 +37,9 @@ namespace UnitTests.Pages.Create
         #endregion TestSetup
 
         #region OnGet
+        /// <summary>
+        /// Test empty OnGet function. Should not change the state of the CreateModel object
+        /// </summary>
         [Test]
         public void OnGet_initial_setup()
         {
@@ -44,6 +53,10 @@ namespace UnitTests.Pages.Create
         #endregion OnGet
 
         #region OnPost
+        /// <summary>
+        /// Test OnPost method when the model state is invalid, thus redirecting
+        /// the user back to the original Create page. 
+        /// </summary>
         [Test]
         public void OnPost_invalidState_returnToPage()
         {
@@ -59,6 +72,11 @@ namespace UnitTests.Pages.Create
 
         }
 
+        /// <summary>
+        /// Test OnPost method when the model state is valid, thus creating
+        /// a new card with the provided Product info. After that, redirecting
+        /// the user back to Admin page to view all available items.
+        /// </summary>
         [Test]
         public void OnPost_ValidState_createCard()
         {
