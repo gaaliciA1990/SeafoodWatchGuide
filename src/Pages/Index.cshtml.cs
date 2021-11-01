21182433a67bdae574ef13d9c5c4bc9b9eb3f79d
khometit <averydinh1996@gmail.com>
2021-11-01 11:34:28 -0700

    Added comments, rearranged code to adhere coding guidelines in Index.cshtml.cs

--- src/Pages/Index.cshtml.cs
+++ src/Pages/Index.cshtml.cs
@@ -13,6 +13,7 @@ namespace ContosoCrafts.WebSite.Pages
     /// </summary>
     public class IndexModel : PageModel
     {
+
         //Logger to help with debugging
         private readonly ILogger<IndexModel> _logger;
 
