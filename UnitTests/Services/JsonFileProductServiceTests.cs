b08d542f4755c4e31d8a53224c63e8312c099a09
khometit <averydinh1996@gmail.com>
2021-11-01 10:35:27 -0700

    Added comments and rearranged code to follow guidelines in JsonFileProductServiceTests.cs

--- UnitTests/Services/JsonFileProductServiceTests.cs
+++ UnitTests/Services/JsonFileProductServiceTests.cs
@@ -9,6 +9,10 @@ using Newtonsoft.Json;
 
 namespace UnitTests.Services
 {
+    /// <summary>
+    /// This class contains tests for the application's supported services
+    /// listed in JsonFileProductService.cs
+    /// </summary>
     class JsonFileProductServiceTests
     {
          #region TestSetup
@@ -18,7 +22,10 @@ namespace UnitTests.Services
         }
         #endregion TestSetup
 
-
+        /// <summary>
+        /// Function tests the CreateCard method using valid state and expecting
+        /// a new card to be created
+        /// </summary>
         [Test]
         public void CreateCard_Valid_Product_Should_Return_True()
         {
@@ -43,13 +50,16 @@ namespace UnitTests.Services
 
             //Assert
             var newCreate = dataSet.FirstOrDefault(m => m.Id.Equals(origin_data.Id));
-
             var origin_data_json= JsonConvert.SerializeObject(origin_data);
             var newCreate_json = JsonConvert.SerializeObject(newCreate);
 
             Assert.AreEqual(origin_data_json, newCreate_json);
         }
 
+        /// <summary>
+        /// Test UpdateCard method using invalid input, the card
+        /// should not be found and therefore not updated.
+        /// </summary>
         [Test]
         public void UpdateCard_inValid_Product_Should_Return_Null()
         {
@@ -74,7 +84,10 @@ namespace UnitTests.Services
             Assert.AreEqual(result, null);
         }
 
-
+        /// <summary>
+        /// Test UpdateCard function using valid input, the card should be
+        /// found and get updated accordingly.
+        /// </summary>
         [Test]
         public void UpdateCard_Valid_Product_Should_Return_Updated_Product()
         {
@@ -96,7 +109,10 @@ namespace UnitTests.Services
             Assert.AreEqual("Test Test Test", updated_data.Description);
         }
 
-
+        /// <summary>
+        /// Test GetRegionData method using invalid input, thus not returning
+        /// anything.
+        /// </summary>
         [Test]
         public void GetRegionData_inValid_Product_Should_Return_Empyt_IEnumerable()
         {
@@ -113,6 +129,10 @@ namespace UnitTests.Services
             Assert.IsEmpty(result);
         }
 
+        /// <summary>
+        /// Test GetRegionData using valid input, thus return the data for the
+        /// specified region accordingly
+        /// </summary>
         [Test]
         public void GetRegionData_Valid_Product_Should_Return_Not_Empty_IEnumerable()
         {
@@ -129,21 +149,22 @@ namespace UnitTests.Services
             Assert.IsNotEmpty(result);
         }
 
+        /// <summary>
+        /// Test GetAllRegions method, should return all supported regions
+        /// </summary>
         [Test]
-        public void GetRegionData_Valid_Product_Should_All_Region()
+        public void GetAllRegions_Should_Return_All_Region()
         {
 
             // Arrange
-            
-
+            var checking = new string[7]{"West Coast", "Southwest", "Central", "Southeast",
+                                "Northeast", "Hawai'i", "National"};
 
             //Act
             var result = TestHelper.ProductService.GetAllRegions();
 
 
             //Assert
-            var checking = new string[7]{"West Coast", "Southwest", "Central", "Southeast",
-                                "Northeast", "Hawai'i", "National"};
             Assert.AreEqual(checking, result);
         }
 
