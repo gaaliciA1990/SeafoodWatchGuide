3eab1ec13b01e76430e4bfed9bfd769515c3d147
Alicia Garcia <garcia.alicia1990@gmail.com>
2021-11-01 22:49:28 -0700

    Created TakeAction page for more information on how to help, TakeAction.cshtml, TakeAction.cshtml.cs

---
+++ src/Pages/TakeAction.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+
+namespace ContosoCrafts.WebSite.Pages
+{
+
+    /// <summary>
+    /// Home page for Seafood Watch Guide from GladiatorMonkys 
+    /// group in Fundamentals of Software Engineering, CPSC 5110
+    /// </summary>
+    public class TakeActionModel : PageModel
+    {
+
+        //Logger to help with debugging
+        private readonly ILogger<TakeActionModel> _logger;
+
+        //To get connected to all provided services
+        public JsonFileProductService ProductService { get; }
+        //To store all products currently in database
+        public IEnumerable<ProductModel> Products { get; private set; }
+
+        /// <summary>
+        /// Constructor for Index page
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="productService"></param>
+        public TakeActionModel(ILogger<TakeActionModel> logger, JsonFileProductService productService)
+        {
+            _logger = logger;
+            ProductService = productService;
+        }
+
+        /// <summary>
+        /// Method to fetch all items to be shown on the Index page
+        /// </summary>
+        public void OnGet()
+        {
+            Products = ProductService.GetAllData();
+        }
+    }
+}
