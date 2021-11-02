70cf02b71c12f9b72a93761d7d8c1f7e0bdb2f62
khometit <averydinh1996@gmail.com>
2021-11-02 12:15:08 -0700

    Added // comments to ProductModel.cs

--- src/Models/ProductModel.cs
+++ src/Models/ProductModel.cs
@@ -8,31 +8,38 @@ using ContosoCrafts.WebSite.Data;
 
 namespace ContosoCrafts.WebSite.Models
 {
-
     /// <summary>
     /// Code version respenting the elements of the JSON file.
     /// </summary>
     public class ProductModel
     {
-
+        //Keep track of the unique ID of the product
         public string Id { get; set; }
 
+
+        //Keep track of which region the product belongs to
         [Required(ErrorMessage = "Please choose a region")]
         public string Region { get; set; }
 
 
+        //Keep track of the image that represents the product
         [JsonPropertyName("img")]
         [Required(ErrorMessage = "Please give a valid url link")]
         [DataType(DataType.Url)]
         public string Image { get; set; }
 
+
+        //Contains the name of the product
         [Required(ErrorMessage = "Please provide a Title")]
         public string Title { get; set; }
 
+
+        //Contains the description of the product
         [Required(ErrorMessage = "Please provide a description")]
         public string Description { get; set; }
 
 
+        //Contains the rating of the product, should be 1 of the specified enums
         [JsonConverter(typeof(JsonStringEnumConverter))]
         [Required(ErrorMessage = "Please choose a rating")]
         public ProductRating? Rating { get; set; }
