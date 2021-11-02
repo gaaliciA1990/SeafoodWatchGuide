9b487f0f80759cc9b09af205a8b94ae8ec6c380d
gaaliciA1990 <32443614+gaaliciA1990@users.noreply.github.com>
2021-11-01 18:40:44 -0700

    Added comments to Program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -3,13 +3,24 @@ using Microsoft.Extensions.Hosting;
 
 namespace ContosoCrafts.WebSite
 {
+    /// <summary>
+    /// This class builds the website
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// This method is the main function that initiates the website build
+        /// </summary>
+        ///  <param name="args"></param>
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
         }
 
+        /// <summary>
+        /// This method builds the website and calls the Startup file
+        /// </summary>
+        ///  <param name="args"></param>
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
