a512be857810e9c833c591fd1c17b32081a4c375
khometit <averydinh1996@gmail.com>
2021-11-02 22:18:26 -0700

    Updated comments to follow guidelines in UnitTest1.cs

--- UnitTests/UnitTest1.cs
+++ UnitTests/UnitTest1.cs
@@ -2,13 +2,22 @@ using NUnit.Framework;
 
 namespace UnitTests
 {
+    /// <summary>
+    /// Class contains several tests
+    /// </summary>
     public class Tests
     {
+        /// <summary>
+        /// Environment set up
+        /// </summary>
         [SetUp]
         public void Setup()
         {
         }
 
+        /// <summary>
+        /// Test function
+        /// </summary>
         [Test]
         public void Test1()
         {
