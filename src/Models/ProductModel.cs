b0104692fd1535f94bb89568ef432432bee0ce5b
Alicia Garcia <garcia.alicia1990@gmail.com>
2021-11-06 17:43:42 -0700

    Removed error messages as they were not appears after new validation implementation for data entry. Added StrintLenght for title and description to ProductModel.cs

--- src/Models/ProductModel.cs
+++ src/Models/ProductModel.cs
@@ -16,33 +16,30 @@ namespace ContosoCrafts.WebSite.Models
         //Keep track of the unique ID of the product
         public string Id { get; set; }
 
-
-        //Keep track of which region the product belongs to
-        [Required(ErrorMessage = "Please choose a region")]
-        public string Region { get; set; }
-
-
-        //Keep track of the image that represents the product
-        [JsonPropertyName("img")]
-        [Required(ErrorMessage = "Please give a valid url link")]
-        [DataType(DataType.Url)]
-        public string Image { get; set; }
-
-
         //Contains the name of the product
-        [Required(ErrorMessage = "Please provide a Title")]
+        [StringLength(15, MinimumLength = 3)]
+        [Required]
         public string Title { get; set; }
 
-
-        //Contains the description of the product
-        [Required(ErrorMessage = "Please provide a description")]
-        public string Description { get; set; }
-
+        //Keep track of which region the product belongs to
+        [Required]
+        public string Region { get; set; }
 
         //Contains the rating of the product, should be 1 of the specified enums
         [JsonConverter(typeof(JsonStringEnumConverter))]
-        [Required(ErrorMessage = "Please choose a rating")]
+        [Required]
         public ProductRating Rating { get; set; }
+
+        //Contains the description of the product
+        [StringLength(120)]
+        [Required]
+        public string Description { get; set; }
+
+        //Keep track of the image that represents the product
+        [JsonPropertyName("img")]
+        [Required]
+        [DataType(DataType.Url)]
+        public string Image { get; set; }
 
         /// <summary>
         /// This will convert the ENUM values to a String format so it's easier for 
