f0173ab1c99b012dc5a9b6dea5dd61e55f15fbe2
khometit <averydinh1996@gmail.com>
2021-11-09 04:59:28 -0800

    Added code to help clear filter data in FilterModel.cs

--- src/Models/FilterModel.cs
+++ src/Models/FilterModel.cs
@@ -3,18 +3,27 @@ using System;
 using System.Text.Json;
 using ContosoCrafts.WebSite.Data;
 
-
-
 namespace ContosoCrafts.WebSite.Models
 {
     /// <summary>
-    /// Code version respenting the elements of the JSON file.
+    /// Code version respenting the elements of the filter
     /// </summary>
     public class FilterModel
     {
-        public string Region;
+        //Store which Region was chosen for filtering
+        public string Region { get; set; }
 
-        public ProductRating Rating;
 
+        //Store which Rating was chosen for filtering
+        public ProductRating Rating { get; set; }
+
+        /// <summary>
+        /// Function to clear all current filter data
+        /// </summary>
+        public void ClearData()
+        {
+            Region = null;
+            Rating = ProductRating.UNKNOWN;
+        }
     }
 }
