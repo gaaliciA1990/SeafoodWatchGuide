6d62b9e71c2c6c8265bc16c33fef6f6e99c74053
khometit <averydinh1996@gmail.com>
2021-11-08 22:45:21 -0800

    Created a model for filters called FilterModel.cs

---
+++ src/Models/FilterModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+using System.Text.Json;
+using ContosoCrafts.WebSite.Data;
+
+
+
+namespace ContosoCrafts.WebSite.Models
+{
+    /// <summary>
+    /// Code version respenting the elements of the JSON file.
+    /// </summary>
+    public class FilterModel
+    {
+        public string Region;
+
+        public ProductRating Rating;
+
+    }
+}
