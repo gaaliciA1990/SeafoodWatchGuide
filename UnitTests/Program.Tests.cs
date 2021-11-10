1f00b41c6b8c92c2c6e089674f5f49f7d1b66fcc
Alicia Garcia <garcia.alicia1990@gmail.com>
2021-11-09 19:45:23 -0800

    Created UT file for Program.cs, but unsure how to implement test for main method, Program.Tests.cs

---
+++ UnitTests/Program.Tests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Hosting;
+using NUnit.Framework;
+
+namespace UnitTests.Program
+{
+    /// <summary>
+    /// This class is testing the Program.cs file to ensure full functionality
+    /// </summary>
+    public class ProgramTests
+    {
+
+    }
+}
