8ea05fd4f8412fdead5f2f28bde1dbd6cda402f1
Alicia Garcia <garcia.alicia1990@gmail.com>
2021-11-11 21:45:44 -0800

    Added Extension Method for the convertToString method to ProductRatingEnum.cs

--- src/Enums/ProductRatingEnum.cs
+++ src/Enums/ProductRatingEnum.cs
@@ -1,4 +1,6 @@
-﻿/// <summary>
+﻿using System;
+
+/// <summary>
 /// This is the ENUM class for seafood ratings, which are displayed on each card and dictate
 /// which section of the guide the fish card is displayed on.
 /// </summary>
@@ -15,5 +17,23 @@ namespace ContosoCrafts.WebSite.RatingEnums
         BEST_CHOICE = 1,
         GOOD_ALTERNATIVE = 2,
         AVOID = 3
+    }
+
+   public static class EnumExtensions
+    {
+        /// <summary>
+        /// This will convert the ENUM values to a String format so it's easier for 
+        /// users to read and not in variable format
+        /// </summary>
+        /// <param name="rating"></param>
+        /// <returns></returns>
+        public static string convertToString(ProductRating rating)
+        {
+            string str = Enum.GetName(rating);
+            str = str.Replace("_", " ");
+            str = char.ToUpper(str[0]) + str.Substring(1).ToLower();
+
+            return str;
+        }
     }
 }
