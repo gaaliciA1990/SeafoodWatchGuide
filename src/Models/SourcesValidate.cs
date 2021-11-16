863c44a5faae2e2f883853ddf53df76324e4d5d1
carlo Provenzani <cprovenzani@seattleu.edu>
2021-11-15 19:42:34 -0800

    Custom validation attribute for Source; Source.Validate.cs

---
+++ src/Models/SourcesValidate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ContosoCrafts.WebSite.Models
+{
+    /// <summary>
+    /// Custom data annotation, check string length in List
+    /// </summary>
+    public class ListStringLength : ValidationAttribute
+    {
+        //max character allowed in Kist item
+        public int MinStringLength { get; set; }
+        //min character allowed in Kist item
+        public int MaxStringLength { get; set; }
+        /// <summary>
+        /// validate list items in value
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public override bool IsValid(object value)
+        {
+            foreach (var str in value as List<string>)
+            {
+                if (str.Length > MaxStringLength || str.Length < MinStringLength) 
+                { 
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
