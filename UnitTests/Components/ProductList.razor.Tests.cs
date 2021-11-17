7cf8251f2a8e6e8a6c728cbdf0f99c1fe1f72be1
khometit <averydinh1996@gmail.com>
2021-11-16 19:59:06 -0800

    Added UT for when a search string is entered, changed ProductList.razor.Tests.cs

--- UnitTests/Components/ProductList.razor.Tests.cs
+++ UnitTests/Components/ProductList.razor.Tests.cs
@@ -66,6 +66,35 @@ namespace UnitTests.Components
 
             Assert.AreEqual(false, button.InnerHtml.Contains("Octopus"));
         }
+
+        /// <summary>
+        /// Pass as part of the parameters to the ProductList Component, the Region of Hawaii, search key as Oysters
+        /// Then find the Div that holds the Best Choice items
+        /// Look inside that inner HTML for that div, looking for the items
+        /// Oysters should be there
+        /// Octopus should not 
+        /// </summary>
+        [Test]
+        public void SearchTitle_Valid_Title_Oysters_Result_Should_Contain_Oysters()
+        {
+            //Arrange
+            Services.AddSingleton<JsonFileProductService>(TestHelper.ProductService);
+
+            var divID = "div_BEST_CHOICE";
+
+            var page = RenderComponent<ProductList>(parameters => parameters.Add(p => p.search, "Oysters").Add(p => p.region, "Hawaii"));
+            var resultTest = page.Markup;
+
+            // Find the Buttons (more info)
+            var buttonList = page.FindAll("Div");
+
+            // Find the one that matches the ID looking for and click it
+            var button = buttonList.First(m => m.OuterHtml.Contains(divID));
+
+            // Assert
+            Assert.AreEqual(true, button.InnerHtml.Contains("Oysters"));
+            Assert.AreEqual(false, button.InnerHtml.Contains("Octopus"));
+        }
         #endregion SelectProduct
     }
 }
