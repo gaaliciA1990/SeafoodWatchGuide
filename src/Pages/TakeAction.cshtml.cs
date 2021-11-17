519041f666135ae9a983634192b2ab71d2b1be90
Alicia Garcia <garcia.alicia1990@gmail.com>
2021-11-16 22:11:42 -0800

    Added model code to debug easier in TakeAction.cshtml.cs

--- src/Pages/TakeAction.cshtml.cs
+++ src/Pages/TakeAction.cshtml.cs
@@ -1,8 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 
 namespace ContosoCrafts.WebSite.Pages
 {
-
     /// <summary>
     /// Take Action page for Seafood Watch Guide to provide site users with
     /// more information on how they can help. Shouldn't need a model, but 
@@ -10,5 +10,23 @@ namespace ContosoCrafts.WebSite.Pages
     /// </summary>
     public class TakeActionModel : PageModel
     {
+        //Logger to help with debugging
+        private readonly ILogger<TakeActionModel> _logger;
+
+        /// <summary>
+        /// Constructor for Privacy page
+        /// </summary>
+        /// <param name="logger"></param>
+        public TakeActionModel(ILogger<TakeActionModel> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// OnGet method for Privacy page. Essentially doing nothing for now
+        /// </summary>
+        public void OnGet()
+        {
+        }
     }
 }
