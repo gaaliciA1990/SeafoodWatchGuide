87b256b1babf2ec357f5b4126bec4e32c9c6c369
gaaliciA1990 <32443614+gaaliciA1990@users.noreply.github.com>
2021-11-17 18:13:51 -0800

    Addressed grading feedback from Sprint6 in StartUp.Tests.cs

--- UnitTests/StartUp.Tests.cs
+++ UnitTests/StartUp.Tests.cs
@@ -3,17 +3,29 @@ using Microsoft.AspNetCore.Hosting;
 
 using NUnit.Framework;
 
+/// <summary>
+/// 
+/// </summary>
 namespace UnitTests.Pages.Startup
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class StartupTests
     {
         #region TestSetup
 
+        /// <summary>
+        /// 
+        /// </summary>
         [SetUp]
         public void TestInitialize()
         {
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
         public class Startup : ContosoCrafts.WebSite.Startup
         {
             public Startup(IConfiguration config) : base(config) { }
@@ -21,6 +33,9 @@ namespace UnitTests.Pages.Startup
         #endregion TestSetup
 
         #region ConfigureServices
+        /// <summary>
+        /// 
+        /// </summary>
         [Test]
         public void Startup_ConfigureServices_Valid_Defaut_Should_Pass()
         {
@@ -30,13 +45,15 @@ namespace UnitTests.Pages.Startup
         #endregion ConfigureServices
 
         #region Configure
+        /// <summary>
+        /// 
+        /// </summary>
         [Test]
         public void Startup_Configure_Valid_Defaut_Should_Pass()
         {
             var webHost = Microsoft.AspNetCore.WebHost.CreateDefaultBuilder().UseStartup<Startup>().Build();
             Assert.IsNotNull(webHost);
         }
-
         #endregion Configure
     }
 }
