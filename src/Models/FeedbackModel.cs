b9e59d5de1e1a79855a65ea0dfb7f65b54fba8b5
Alicia Garcia <garcia.alicia1990@gmail.com>
2021-11-20 16:13:29 -0800

    Created new FeedbackModel for accepting ratings and comments for our website, FeedbackModel.cs

---
+++ src/Models/FeedbackModel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using ContosoCrafts.WebSite.RatingEnums;
+
+namespace ContosoCrafts.WebSite.Models
+{
+    /// <summary>
+    /// Code version respenting the elements of the feedback JSON file
+    /// </summary>
+    public class FeedbackModel
+    {
+        // Store rating ID
+        public string ID { get; set; }
+
+        //Store the rating value
+        [Required]
+        public int[] Rating { get; set; }
+
+        // Store the comments entered by users for the site feedback
+        [StringLength (maximumLength:150, MinimumLength = 3, ErrorMessage = "Comments should have a length of more than {2} and less than {1}")]
+        public string Comment { get; set; }
+
+        /// <summary>
+        /// The is a toString method that will convert this class (the model) to a string version. 
+        /// This is using the JSON mechanism for serializing the model data to a string
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString() => JsonSerializer.Serialize<FeedbackModel>(this);
+    }
+}
