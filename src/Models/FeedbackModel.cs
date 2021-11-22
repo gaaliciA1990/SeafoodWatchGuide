8a6dc9c2431a2944478d785f722013ddbe71b5e9
Alicia Garcia <garcia.alicia1990@gmail.com>
2021-11-21 18:20:45 -0800

    Updated the Max string length to 500 in FeedbackModel.cs

--- src/Models/FeedbackModel.cs
+++ src/Models/FeedbackModel.cs
@@ -19,7 +19,7 @@ namespace ContosoCrafts.WebSite.Models
         public int Rating { get; set; }
 
         // Store the comments entered by users for the site feedback
-        [StringLength (maximumLength:150, MinimumLength = 3, ErrorMessage = "Comments should have a length of more than {2} and less than {1}")]
+        [StringLength (maximumLength:500, MinimumLength = 3, ErrorMessage = "Comments should have a length of more than {2} and less than {1}")]
         public string Comment { get; set; }
 
         /// <summary>
