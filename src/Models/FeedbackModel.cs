8a65ed4407096dd1319c004a5cca0a77f61b9d9f
gaaliciA1990 <32443614+gaaliciA1990@users.noreply.github.com>
2021-11-22 15:22:33 -0800

    Removed unused using statement from FeedbackModel.cs

--- src/Models/FeedbackModel.cs
+++ src/Models/FeedbackModel.cs
@@ -2,7 +2,6 @@
 using System.ComponentModel.DataAnnotations;
 using System.Text.Json;
 using System.Text.Json.Serialization;
-using ContosoCrafts.WebSite.RatingEnums;
 
 namespace ContosoCrafts.WebSite.Models
 {
