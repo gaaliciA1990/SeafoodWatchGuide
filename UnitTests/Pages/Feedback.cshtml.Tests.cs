a4e5e8fd49a48faf24cf4f2bd795d2eb7ea2d0fe
khometit <averydinh1996@gmail.com>
2021-11-23 02:55:39 -0800

    Added UTs for OnPost function in Feedback.cshtml.Tests.cs

--- UnitTests/Pages/Feedback.cshtml.Tests.cs
+++ UnitTests/Pages/Feedback.cshtml.Tests.cs
@@ -1,4 +1,5 @@
 using System.Linq;
+using ContosoCrafts.WebSite.Models;
 using Microsoft.Extensions.Logging;
 using NUnit.Framework;
 using ContosoCrafts.WebSite.Pages;
@@ -7,14 +8,14 @@ using Moq;
 namespace UnitTests.Pages.Feedback
 {
     /// <summary>
-    /// Class containing all unit tests for Index page
+    /// Class containing all unit tests for Feedback page
     /// </summary>
     public class FeedbackTests
     {
 
         #region TestSetup
-        //pageModel represents the Index model used to get tested
-        public static IndexModel PageModel;
+        //pageModel represents the Feedback model used to get tested
+        public static FeedbackPageModel PageModel;
 
         /// <summary>
         /// Test constructor
@@ -22,9 +23,9 @@ namespace UnitTests.Pages.Feedback
         [SetUp]
         public void TestInitialize()
         {
-            var MockLoggerDirect = Mock.Of<ILogger<IndexModel>>();
+            var MockLoggerDirect = Mock.Of<ILogger<FeedbackPageModel>>();
 
-            PageModel = new IndexModel(MockLoggerDirect, TestHelper.ProductService)
+            PageModel = new FeedbackPageModel(MockLoggerDirect, TestHelper.FeedbackService)
             {
             };
         }
@@ -44,7 +45,68 @@ namespace UnitTests.Pages.Feedback
 
             // Assert
             Assert.AreEqual(true, PageModel.ModelState.IsValid);
+            Assert.AreEqual(false, PageModel.displayThankYouMessage);
         }
         #endregion OnGet
+
+        #region OnPost
+        /// <summary>
+        /// Test OnPost function with valid states, should return valid model
+        /// state and display flag should be turned to true
+        /// </summary>
+        [Test]
+        public void OnPost_Valid_Input_Should_Return_Valid_Model_State()
+        {
+            //Arrange
+            PageModel.Feedback = new FeedbackModel
+            {
+                ID = "123",
+                Rating = 5,
+                Comment = "test comment"
+            };
+
+            bool beforeOnPost = PageModel.displayThankYouMessage;
+
+
+            //Act
+            var result = PageModel.OnPost();
+
+            //Reset
+
+            //Assert
+            Assert.AreEqual(true, PageModel.ModelState.IsValid);
+            Assert.AreEqual(true, PageModel.displayThankYouMessage);
+            Assert.AreEqual(false, beforeOnPost);
+        }
+
+        /// <summary>
+        /// Test OnPost function with invalid states, should return invalid model
+        /// state and display flag should be turned to false
+        /// </summary>
+        [Test]
+        public void OnPost_Invalid_Rating_Null_Should_Return_Invalid_Model_State()
+        {
+            //Arrange
+            PageModel.Feedback = new FeedbackModel
+            {
+                ID = "123",
+                Comment = "test comment"
+            };
+
+            bool beforeOnPost = PageModel.displayThankYouMessage;
+            PageModel.ModelState.AddModelError("bogus", "bogus error");
+
+            //Act
+            var result = PageModel.OnPost();
+
+            //Reset
+
+            //Assert
+            Assert.AreEqual(false, PageModel.ModelState.IsValid);
+            Assert.AreEqual(false, beforeOnPost);
+            Assert.AreEqual(false, PageModel.displayThankYouMessage);
+            
+        }
+        #endregion OnPost
     }
 }
