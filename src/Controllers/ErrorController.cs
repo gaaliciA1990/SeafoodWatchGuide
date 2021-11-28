1291337971c7678ce7f68c2154a6b0e0a46d2f14
gaaliciA1990 <32443614+gaaliciA1990@users.noreply.github.com>
2021-11-27 16:58:57 -0800

    Updated method name on line 8 in ErrorController.cs

--- src/Controllers/ErrorController.cs
+++ src/Controllers/ErrorController.cs
@@ -1,28 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using ContosoCrafts.WebSite.Models;
-using ContosoCrafts.WebSite.Services;
-using Microsoft.Extensions.Logging;
 
 namespace ContosoCrafts.WebSite.Controllers
 {
     public class ErrorController : Controller
     {
         [Route("Error/{statusCode}")]
-        public IActionResult HttpStatusCodeHandler(int statusCode)
+        public IActionResult CodeHandler(int statusCode)
         {
             switch (statusCode)
             {
                 case 404:
                     return RedirectToPage("/NotFound");
-
-                case 500:
-                    return RedirectToPage("/Error");
-
                 default:
                     return RedirectToPage("/Error");
             }
