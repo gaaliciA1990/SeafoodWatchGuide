b5a93be97b218255abe756d8f801ec512990aec9
gaaliciA1990 <32443614+gaaliciA1990@users.noreply.github.com>
2021-11-27 16:26:59 -0800

    Created a new page for 404 error, NotFound.cshtml, NotFound.cshtml.cs

---
+++ src/Pages/NotFound.cshtml.cs
@@ -0,0 +1,40 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace ContosoCrafts.WebSite.Pages
+{
+    /// <summary>
+    /// Model for error page
+    /// </summary>
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public class NotFoundModel : PageModel
+    {
+        //Variable to hold the ID of the error request
+        public string RequestId { get; set; }
+
+        //Variable to specify whether to show the request ID or not
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+        //Logger to help with debugging when there is an error
+        private readonly ILogger<NotFoundModel> _logger;
+
+        /// <summary>
+        /// Constructor for Error page
+        /// </summary>
+        /// <param name="logger"></param>
+        public NotFoundModel(ILogger<NotFoundModel> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Method to fetch error stack trace
+        /// </summary>
+        public void OnGet()
+        {
+            RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+        }
+    }
+}
